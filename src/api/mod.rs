b95d4284a906c557847e69b66f9cd3f2cd94f8b9
Xiphoseer <xiphoseer@mailbox.org>
2021-09-11 21:33:47 +0200

    Add 'all' for locale and tables

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -2,11 +2,18 @@ use std::{borrow::Cow, convert::Infallible, error::Error, sync::Arc};
 
 use assembly_core::buffer::CastError;
 use assembly_data::{
-    fdb::{common::ValueType, mem::Database, query},
+    fdb::{
+        common::ValueType,
+        mem::{Column, Database, Row},
+        query,
+    },
     xml::localization::LocaleNode,
 };
 use linked_hash_map::LinkedHashMap;
-use serde::Serialize;
+use serde::{
+    ser::{SerializeMap, SerializeSeq},
+    Serialize,
+};
 use warp::{
     path::Tail,
     reply::{Json, WithStatus},
@@ -146,49 +153,81 @@ fn table_def_api(db: Database<'_>, name: String) -> Result<Option<Json>, CastErr
     }
 }
 
+struct RowIter<'a, C, R, FR>
+where
+    C: Iterator<Item = Column<'a>> + Clone,
+    R: Iterator<Item = Row<'a>>,
+    FR: Fn() -> R,
+{
+    cols: C,
+    to_rows: FR,
+}
+
+impl<'a, C, R, FR> Serialize for RowIter<'a, C, R, FR>
+where
+    C: Iterator<Item = Column<'a>> + Clone,
+    R: Iterator<Item = Row<'a>>,
+    FR: Fn() -> R,
+{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut s = serializer.serialize_seq(None)?;
+        for r in (self.to_rows)() {
+            let mut row = LinkedHashMap::new();
+            let mut fields = r.field_iter();
+            for col in self.cols.clone() {
+                let col_name = col.name();
+                let field = fields.next().unwrap();
+                row.insert(col_name, field);
+            }
+            s.serialize_element(&row)?;
+        }
+        s.end()
+    }
+}
+
+fn table_all_api(db: Database<'_>, name: String) -> Result<Option<Json>, CastError> {
+    let tables = db.tables()?;
+    let table = match tables.by_name(&name) {
+        Some(t) => t?,
+        None => return Ok(None),
+    };
+
+    let cols = table.column_iter();
+    let to_rows = || table.row_iter().take(100);
+
+    Ok(Some(warp::reply::json(&RowIter { cols, to_rows })))
+}
+
 fn table_key_api(db: Database<'_>, name: String, key: String) -> Result<Option<Json>, CastError> {
     let tables = db.tables()?;
-    if let Some(table) = tables.by_name(&name) {
-        let table = table?;
-        let mut cols = table.column_iter();
-        let index_field = cols.next().unwrap();
-        let index_field_type = index_field.value_type();
-        let index_name = index_field.name();
+    let table = match tables.by_name(&name) {
+        Some(t) => t?,
+        None => return Ok(None),
+    };
 
-        match query::pk_filter(key, index_field_type) {
-            Ok(pk_filter) => {
-                let bucket_index = pk_filter.hash() as usize % table.bucket_count();
-                let bucket = table.bucket_at(bucket_index).unwrap();
-                let mut rows = Vec::new();
+    let index_field = table.column_at(0).unwrap();
+    let index_field_type = index_field.value_type();
 
-                for row in bucket.row_iter() {
-                    let mut field_iter = row.field_iter();
-                    let index_mem = field_iter.next().unwrap();
-                    if pk_filter.filter(&index_mem) {
-                        let mut row = LinkedHashMap::new();
-                        row.insert(index_name.clone(), index_mem);
-                        // add the remaining fields
-                        #[allow(clippy::clone_on_copy)]
-                        for col in cols.clone() {
-                            let col_name = col.name();
-                            let field = field_iter.next().unwrap();
-                            row.insert(col_name, field);
-                        }
-                        rows.push(row);
-                    }
-                }
-                Ok(Some(warp::reply::json(&rows)))
-            }
-            Err(_e) => Ok(None),
-        }
-    } else {
-        Ok(None)
-    }
+    let pk_filter = match query::pk_filter(key, index_field_type) {
+        Ok(f) => f,
+        Err(_) => return Ok(None),
+    };
 
-    //let lname = Latin1String::encode(&name);
-    //let db_table = db_table.as_bytes_handle();
+    let bucket_index = pk_filter.hash() as usize % table.bucket_count();
+    let bucket = table.bucket_at(bucket_index).unwrap();
 
-    //wrap_200(table_index(db_table, lname.as_ref(), key))
+    let filter = &pk_filter;
+    let cols = table.column_iter();
+    let to_rows = || {
+        return bucket
+            .row_iter()
+            .filter(move |r| filter.filter(&r.field_at(0).unwrap()));
+    };
+
+    Ok(Some(warp::reply::json(&RowIter { cols, to_rows })))
 }
 
 fn wrap_404<A: Reply>(reply: A) -> WithStatus<A> {
@@ -230,13 +269,25 @@ fn make_api_tables(
         .and(warp::path("def"))
         .map(table_def_api)
         .map(map_opt_res);
+    // The `/tables/:name/all` endpoint
+    let table_all = table
+        .clone()
+        .and(warp::path("all"))
+        .map(table_all_api)
+        .map(map_opt_res);
     // The `/tables/:name/:key` endpoint
     let table_get = table
         .and(warp::path::param())
         .map(table_key_api)
         .map(map_opt_res);
 
-    tables.or(table_def).unify().or(table_get).unify()
+    tables
+        .or(table_def)
+        .unify()
+        .or(table_all)
+        .unify()
+        .or(table_get)
+        .unify()
 }
 
 /*fn copy_filter<'x, T>(v: T) -> impl Filter<Extract = (T,), Error=Infallible> + Clone + 'x where T: Send + Sync + Copy + 'x {
@@ -262,6 +313,40 @@ struct LocalePod<'a> {
     str_keys: Vec<&'a str>,
 }
 
+struct LocaleAll<'a> {
+    inner: &'a LocaleNode,
+}
+
+impl<'a> Serialize for LocaleAll<'a> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let v_count = if self.inner.value.is_some() { 1 } else { 0 };
+        let i_count = self.inner.int_children.len();
+        let s_count = self.inner.str_children.len();
+        let count = v_count + i_count + s_count;
+
+        if i_count + s_count > 0 {
+            let mut m = serializer.serialize_map(Some(count))?;
+            if let Some(v) = &self.inner.value {
+                m.serialize_entry(&"$value", v)?;
+            }
+            for (key, inner) in &self.inner.int_children {
+                m.serialize_entry(key, &Self { inner })?;
+            }
+            for (key, inner) in &self.inner.str_children {
+                m.serialize_entry(key, &Self { inner })?;
+            }
+            m.end()
+        } else if let Some(v) = &self.inner.value {
+            serializer.serialize_str(v)
+        } else {
+            serializer.serialize_none()
+        }
+    }
+}
+
 pub(crate) fn make_api<'a>(
     db: Database<'a>,
     tydb: &'static TypedDatabase<'a>,
@@ -279,7 +364,16 @@ pub(crate) fn make_api<'a>(
         .map(move |p: Tail| {
             let path = p.as_str().trim_end_matches('/');
             let mut node = lr.as_ref();
+            let mut all = false;
             if !path.is_empty() {
+                let path = match path.strip_suffix("/$all") {
+                    Some(prefix) => {
+                        all = true;
+                        prefix
+                    }
+                    None => path,
+                };
+
                 // Skip loop for root node
                 for seg in path.split('/') {
                     if let Some(new) = {
@@ -295,11 +389,15 @@ pub(crate) fn make_api<'a>(
                     }
                 }
             }
-            Some(warp::reply::json(&LocalePod {
-                value: node.value.as_deref(),
-                int_keys: node.int_children.keys().cloned().collect(),
-                str_keys: node.str_children.keys().map(|s| s.as_ref()).collect(),
-            }))
+            if all {
+                Some(warp::reply::json(&LocaleAll { inner: node }))
+            } else {
+                Some(warp::reply::json(&LocalePod {
+                    value: node.value.as_deref(),
+                    int_keys: node.int_children.keys().cloned().collect(),
+                    str_keys: node.str_children.keys().map(|s| s.as_ref()).collect(),
+                }))
+            }
         })
         .map(map_opt);
 
