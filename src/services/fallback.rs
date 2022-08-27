144178db61bf663d1a462755957751daaaada2a8
Xiphoseer <xiphoseer@mailbox.org>
2022-08-27 22:08:58 +0200

    Simplify Fallback Service

--- src/services/fallback.rs
+++ src/services/fallback.rs
@@ -5,7 +5,10 @@ use std::{
     task::{Context, Poll},
 };
 
-use http::uri::{self, PathAndQuery};
+use http::{
+    uri::{self, PathAndQuery},
+    Request as HttpRequest, Response as HttpResponse, Uri,
+};
 use hyper::body::Bytes;
 use tower::Service;
 use tower_http::services::{
@@ -17,55 +20,52 @@ use tower_http::services::{
 /// do itself right now, but actually doesn't
 #[derive(Clone)]
 pub struct FallbackService {
-    maps: ServeDir,
-    scripts: ServeDir,
+    inner: ServeDir,
 }
 
 impl FallbackService {
     pub fn new(lu_json_path: &Path) -> Self {
         Self {
-            maps: ServeDir::new(lu_json_path.join("maps")),
-            scripts: ServeDir::new(lu_json_path.join("scripts")),
+            inner: ServeDir::new(lu_json_path),
         }
     }
 
+    /// The routes that require the fallback
     pub(super) fn requires_fallback(path: &str) -> bool {
         path.starts_with("/api/v0/maps/") || path.starts_with("/api/v0/scripts/")
     }
+
+    /// The prefix path to remove from the request before passing to [ServeDir]
+    const PREFIX: &'static str = "/api/v0";
 }
 
-impl<B> Service<http::Request<B>> for FallbackService
+impl<B> Service<HttpRequest<B>> for FallbackService
 where
     B: http_body::Body<Data = Bytes> + Send + 'static,
     B::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
 {
-    type Response = http::Response<ServeFileSystemResponseBody>;
+    type Response = HttpResponse<ServeFileSystemResponseBody>;
     type Error = io::Error;
     type Future = ServeFileSystemResponseFuture<B, DefaultServeDirFallback>;
 
-    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        // [ServeDir] is always ready
-        Poll::Ready(Ok(()))
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        <ServeDir as Service<HttpRequest<B>>>::poll_ready(&mut self.inner, cx)
     }
 
-    fn call(&mut self, req: http::Request<B>) -> Self::Future {
+    fn call(&mut self, req: HttpRequest<B>) -> Self::Future {
         let path = req.uri().path();
         let mut parts = uri::Parts::default();
         parts.authority = req.uri().authority().cloned();
         parts.scheme = req.uri().scheme().cloned();
 
-        let (handler, path) = if let Some(suffix) = path.strip_prefix("/api/v0/maps") {
-            (&mut self.maps, suffix)
-        } else if let Some(suffix) = path.strip_prefix("/api/v0/scripts") {
-            (&mut self.scripts, suffix)
-        } else {
-            panic!("Should not reach this for other paths")
-        };
-        parts.path_and_query = Some(PathAndQuery::from_str(path).unwrap());
+        let suffix = path
+            .strip_prefix(Self::PREFIX)
+            .expect("Should not reach this for other paths");
+        parts.path_and_query = Some(PathAndQuery::from_str(suffix).unwrap());
         let (mut req_parts, body) = req.into_parts();
-        req_parts.uri = http::Uri::from_parts(parts).unwrap();
-        let req = http::Request::from_parts(req_parts, body);
+        req_parts.uri = Uri::from_parts(parts).unwrap();
+        let req = HttpRequest::from_parts(req_parts, body);
 
-        handler.call(req)
+        self.inner.call(req)
     }
 }
